d3cc453b72ae33b240855d728dcc9fbd32486a42
jylipanpan <lz_lipanpan@163.com>
2022-04-02 13:47:07 +0800

    这是李盼盼第一次提交

---
+++ src/main/java/cn/xj/controller/addcontroller.java
@@ -0,0 +1,12 @@
+package cn.xj.controller;
+
+public class addcontroller {
+    public void addlogin(){
+        System.out.println("xue添加了一个登录功能");
+        System.out.println("这是在dev分支下修改的内容");
+        System.out.println("组员裴信洁提交了");
+        System.out.println("组员裴信洁第二次提交了");
+        System.out.println("克隆最新版，然后再修改提交，注意解决冲突！！！");
+        System.out.println("组员李盼盼提交了");
+    }
+}
